eb7dc85721c5a5245a4f0e705b499734db6d75e2
GustavoHeimburg <gutogustavogu@gmail.com>
2023-11-17 10:23:51 -0300

    Iniciando projeto

---
+++ src/test/java/com/example/desafiosspring/DesafiosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
