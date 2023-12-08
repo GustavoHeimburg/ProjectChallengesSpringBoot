c08900e1d12aed40d953402a1575f3178a880bda
GustavoHeimburg <gutogustavogu@gmail.com>
2023-12-08 10:46:44 -0300

    Iniciando de vez

---
+++ src/main/java/com/example/desafiosspring/MyController.java
@@ -0,0 +1,15 @@
+package com.example.desafiosspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/index")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from Spring Boot!");
+        return "index";
+    }
+}
