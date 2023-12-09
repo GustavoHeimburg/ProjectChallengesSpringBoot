b0545d341742dc295db08b3241ce59a005ad7e2c
GustavoHeimburg <gutogustavogu@gmail.com>
2023-12-08 22:17:24 -0300

    tentativa de arrumar bootstrap

--- src/main/java/com/example/desafiosspring/DesafiosSpringApplication.java
+++ src/main/java/com/example/desafiosspring/DesafiosSpringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesafiosSpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
         SpringApplication.run(DesafiosSpringApplication.class, args);
     }
 
